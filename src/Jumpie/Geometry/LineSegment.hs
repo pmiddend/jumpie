eb4557ec1147ad94718292ae0607cfba47c2a5f0
Philipp Middendorf <pmidden+github@secure.mailbox.org>
2014-05-04 14:24:20 +0200

    Fix line segment compilation bug in gcc-4.7

--- src/Jumpie/Geometry/LineSegment.hs
+++ src/Jumpie/Geometry/LineSegment.hs
@@ -9,5 +9,5 @@ data LineSegment a = LineSegment { from :: a,to :: a }
 instance Functor LineSegment where
   fmap f (LineSegment a b) = LineSegment (f a) (f b)
 
-pointList :: LineSegment a => [a]
+pointList :: LineSegment a -> [a]
 pointList (LineSegment a b) = [a,b]
